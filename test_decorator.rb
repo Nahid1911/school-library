5305e5c89c7a2b8df0c224b02e0c5ddd5e887f30
Nahid1911 <nahid.raihan@gmail.com>
2023-08-03 01:39:01 +0600

    Correct linters

--- test_decorator.rb
+++ test_decorator.rb
@@ -6,16 +6,9 @@ require_relative 'student'
 require_relative 'book'
 require_relative 'rental'
 
-
 person = Person.new(22, 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
-
-
-Student.new(5, 'Hia')
-Student.new(2, math_class, 'Hima')
-Student.new(28, math_class, 'Chaity')
-Classroom.new('Math Class')
