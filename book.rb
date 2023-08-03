00549de4edae00f71606b2554283c76a9e545994
Nahid1911 <nahid.raihan@gmail.com>
2023-08-03 09:59:09 +0600

    Refactor add_rentals method in book.rb

--- book.rb
+++ book.rb
@@ -7,7 +7,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals << rental
+  def add_rental(date, person)
+    @rentals.push(Rental.new(date, self, person)) unless @rentals.include?(Rental.new(date, self, person))
   end
 end
